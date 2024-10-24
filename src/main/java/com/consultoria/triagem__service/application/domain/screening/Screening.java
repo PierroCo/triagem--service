bb5f86c0fa63629a1bc0804becfdc4bb432d8708
WalyferRamos <almeidawalyfer@gmail.com>
2024-10-23 21:07:01 -0300

    feat: create class Screening and update UserCase

--- src/main/java/com/consultoria/triagem__service/application/domain/screening/Screening.java
+++ src/main/java/com/consultoria/triagem__service/application/domain/screening/Screening.java
@@ -1,8 +1,15 @@
 package com.consultoria.triagem__service.application.domain.screening;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Screening {
     UUID id;
     UUID userId;
@@ -14,7 +21,20 @@ public class Screening {
     String desiredPosition;
     Date timeToAchieveGoal;
 
-    public Screening(UUID id, UUID userId, String profession, String linkedin, String activityArea, String currentPosition, String areaOfInterest, String desiredPosition, Date timeToAchieveGoal) {
+    public Screening(final String profession, final String linkedin, final String activityArea,
+                     final String currentPosition, final String areaOfInterest, final String desiredPosition, final Date timeToAchieveGoal) {
+        this.profession = profession;
+        this.linkedin = linkedin;
+        this.activityArea = activityArea;
+        this.currentPosition = currentPosition;
+        this.areaOfInterest = areaOfInterest;
+        this.desiredPosition = desiredPosition;
+        this.timeToAchieveGoal = timeToAchieveGoal;
+    }
+
+    public Screening(final UUID id, final UUID userId, final String profession, final String linkedin, final String activityArea,
+                     final String currentPosition, final String areaOfInterest, final String desiredPosition, final Date timeToAchieveGoal) {
+
         this.id = id;
         this.userId = userId;
         this.profession = profession;
@@ -26,75 +46,27 @@ public class Screening {
         this.timeToAchieveGoal = timeToAchieveGoal;
     }
 
-    public UUID getId() {
-        return id;
+    public static Screening newScreening(final String profession, final String linkedin, final String activityArea,
+                                         final String currentPosition, final String areaOfInterest, final String desiredPosition, final Date timeToAchieveGoal) {
+        return new Screening(profession, linkedin, activityArea, currentPosition, areaOfInterest, desiredPosition, timeToAchieveGoal);
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
-
-    public String getProfession() {
-        return profession;
-    }
-
-    public void setProfession(String profession) {
+    public Screening update(final String profession, final String linkedin, final String activityArea,
+                            final String currentPosition, final String areaOfInterest, final String desiredPosition, final Date timeToAchieveGoal) {
         this.profession = profession;
-    }
-
-    public String getLinkedin() {
-        return linkedin;
-    }
-
-    public void setLinkedin(String linkedin) {
         this.linkedin = linkedin;
-    }
-
-    public String getActivityArea() {
-        return activityArea;
-    }
-
-    public void setActivityArea(String activityArea) {
         this.activityArea = activityArea;
-    }
-
-    public String getCurrentPosition() {
-        return currentPosition;
-    }
-
-    public void setCurrentPosition(String currentPosition) {
         this.currentPosition = currentPosition;
-    }
-
-    public String getAreaOfInterest() {
-        return areaOfInterest;
-    }
-
-    public void setAreaOfInterest(String areaOfInterest) {
         this.areaOfInterest = areaOfInterest;
-    }
-
-    public String getDesiredPosition() {
-        return desiredPosition;
-    }
-
-    public void setDesiredPosition(String desiredPosition) {
         this.desiredPosition = desiredPosition;
-    }
-
-    public Date getTimeToAchieveGoal() {
-        return timeToAchieveGoal;
-    }
-
-    public void setTimeToAchieveGoal(Date timeToAchieveGoal) {
         this.timeToAchieveGoal = timeToAchieveGoal;
+
+        return this;
+    }
+    public static Screening with(final UUID id, final UUID userId, final String profession, final String linkedin, final String activityArea,
+                                 final String currentPosition, final String areaOfInterest, final String desiredPosition, final Date timeToAchieveGoal){
+        return new Screening(id, userId,profession, linkedin, activityArea, currentPosition, areaOfInterest, desiredPosition, timeToAchieveGoal);
     }
 }
+
+
