b5907ba8f09d1521a1517475cf0865b5679a3213
Leticia Orozimbo <leticia-dospassos7@hotmail.com>
2024-10-25 18:49:01 -0300

    update useCases

--- src/main/java/com/consultoria/triagem__service/application/useCases/ScreeningDeleteUseCase.java
+++ src/main/java/com/consultoria/triagem__service/application/useCases/ScreeningDeleteUseCase.java
@@ -1,4 +1,18 @@
 package com.consultoria.triagem__service.application.useCases;
 
+import com.consultoria.triagem__service.application.gateway.ScreeningGateway;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class ScreeningDeleteUseCase {
+
+    @NonNull
+    private final ScreeningGateway screeningGateway;
+
+    public void execute(final UUID id) {
+        screeningGateway.deleteById(id);
+    }
 }
--- src/main/java/com/consultoria/triagem__service/application/useCases/ScreeningGetByIdUseCase.java
+++ src/main/java/com/consultoria/triagem__service/application/useCases/ScreeningGetByIdUseCase.java
@@ -1,4 +1,40 @@
 package com.consultoria.triagem__service.application.useCases;
 
+import com.consultoria.triagem__service.application.domain.exceptions.NotFoundException;
+import com.consultoria.triagem__service.application.domain.screening.Screening;
+import com.consultoria.triagem__service.application.gateway.ScreeningGateway;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class ScreeningGetByIdUseCase {
+
+    @NonNull
+    private final ScreeningGateway screeningGateway;
+
+    public Output execute(final UUID id) {
+        return screeningGateway.findById(id)
+                .map(Output::from)
+                .orElseThrow(() -> new NotFoundException("Screening not found"));
+
+    }
+
+    public record Output(UUID id, UUID userId, String profession, String linkedin, String activityArea,
+                         String currentPosition, String areaOfInterest, String desiredPosition,
+                         String position, Date timeToAchieveGoal) {
+
+        public static Output from(Screening screening) {
+            return new Output(screening.getId(), screening.getUserId(), screening.getProfession(), screening.getLinkedin(), screening.getActivityArea(),
+                    screening.getCurrentPosition(), screening.getCurrentPosition(), screening.getAreaOfInterest(), screening.getDesiredPosition(), screening.getTimeToAchieveGoal());
+        }
+    }
 }
+
+
+
+
+
+
